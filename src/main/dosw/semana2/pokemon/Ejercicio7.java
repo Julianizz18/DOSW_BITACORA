93869281135bfd3e4401347118f827461e8b841a
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:12:22 -0500

    Hice el ejercicio 7 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio7.java
+++ src/main/dosw/semana2/pokemon/Ejercicio7.java
@@ -1,4 +1,16 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        List<String> desordenados = Arrays.asList(
+                "Gengar", "Alakazam", "Arcanine",
+                "Golem", "Machamp", "Doduo"
+        );
+        List<String> pokedexOrdenada = desordenados.stream()
+                .sorted()
+                .toList();
+        System.out.println("Pokédex ordenada: " + pokedexOrdenada);
+    }
 }
