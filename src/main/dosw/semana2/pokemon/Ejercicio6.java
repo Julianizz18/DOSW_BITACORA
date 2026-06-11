b33e13bf342c9f16e8aa29b953c62df1726e2f3e
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:11:31 -0500

    Hice el ejercicio 6 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio6.java
+++ src/main/dosw/semana2/pokemon/Ejercicio6.java
@@ -1,4 +1,16 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        List<String> capturados = Arrays.asList(
+                "Gengar", "Arcanine", "Gengar",
+                "Alakazam", "Arcanine", "Golem"
+        );
+        List<String> pokedexUnica = capturados.stream()
+                .distinct()
+                .toList();
+        System.out.println("Pokédex sin duplicados: " + pokedexUnica);
+    }
 }
