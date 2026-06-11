e4bc8d533e7063161082e3a885bf28a0378f9c5b
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:17:54 -0500

    Hice el ejercicio 10 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio10.java
+++ src/main/dosw/semana2/pokemon/Ejercicio10.java
@@ -1,4 +1,19 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
 public class Ejercicio10 {
+    public static void main(String[] args) {
+        List<Pokemon> pokedex = Arrays.asList(
+                new Pokemon(1L, "Alakazam", "Psíquico",  55, 445, "Kanto", false),
+                new Pokemon(2L, "Articuno", "Hielo",     75, 580, "Kanto", true),
+                new Pokemon(3L, "Machamp",  "Lucha",     48, 390, "Kanto", false),
+                new Pokemon(4L, "Zapdos",   "Eléctrico", 78, 620, "Kanto", true),
+                new Pokemon(5L, "Geodude",  "Roca",      22, 175, "Kanto", false),
+                new Pokemon(6L, "Arcanine", "Fuego",     65, 555, "Kanto", false)
+        );
+        List<String> soloNombres = pokedex.stream()
+                .map(Pokemon::getNombre)
+                .toList();
+        System.out.println("Pokédex Compacta: " + soloNombres);
+    }
 }
