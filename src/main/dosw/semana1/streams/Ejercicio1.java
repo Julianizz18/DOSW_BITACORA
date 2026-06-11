22377d38ea1e7b3de3b0b83340d3c935e9c3bb24
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 10:51:08 -0500

    Hice el ejercicio 1 de la semana 1

--- src/main/dosw/semana1/streams/Ejercicio1.java
+++ src/main/dosw/semana1/streams/Ejercicio1.java
@@ -1,1 +1,15 @@
-alo
+package main.dosw.semana1.streams;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        List<Integer> valores = Arrays.asList(5, 14, 7, 22, 9, 16, 30, 11);
+        List<Integer> paresMayoresDiez = valores.stream()
+                .filter(n -> n > 10)
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println("Resultado: " + paresMayoresDiez);
+    }
+}
