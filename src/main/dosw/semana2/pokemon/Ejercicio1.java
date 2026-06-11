763b9de01bb6923e0fa78d00b0ba273ec7b8f10a
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:02:47 -0500

    Hice el ejercicio 1 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio1.java
+++ src/main/dosw/semana2/pokemon/Ejercicio1.java
@@ -1,1 +1,21 @@
-
+package main.dosw.semana2.pokemon;
+import java.util.Arrays;
+import java.util.List;
+public class Ejercicio1 {
+    record Criatura(String nombre, String elemento) {}
+    public static void main(String[] args) {
+        List<Criatura> pokedex = Arrays.asList(
+                new Criatura("Charizard",  "Fuego"),
+                new Criatura("Blastoise",  "Agua"),
+                new Criatura("Arcanine",   "Fuego"),
+                new Criatura("Venusaur",   "Planta"),
+                new Criatura("Rapidash",   "Fuego"),
+                new Criatura("Vaporeon",   "Agua")
+        );
+        List<String> tipoFuego = pokedex.stream()
+                .filter(c -> c.elemento().equals("Fuego"))
+                .map(Criatura::nombre)
+                .toList();
+        System.out.println("Pokémon tipo Fuego: " + tipoFuego);
+    }
+}
