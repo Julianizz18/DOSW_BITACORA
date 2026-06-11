23f587e50239338ef7a96093b6a7de29e5d81ba6
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:24:47 -0500

    Hice el ejercicio 16 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio16.java
+++ src/main/dosw/semana2/pokemon/Ejercicio16.java
@@ -1,4 +1,21 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Ejercicio16 {
+    public static void main(String[] args) {
+        List<Entrenador> competidores = Arrays.asList(
+                new Entrenador(1L, "Red",     9,  List.of()),
+                new Entrenador(2L, "Blue",    7,  List.of()),
+                new Entrenador(3L, "Silver",  4,  List.of()),
+                new Entrenador(4L, "Lance",   11, List.of()),
+                new Entrenador(5L, "Kris",    3,  List.of()),
+                new Entrenador(6L, "Ethan",   8,  List.of())
+        );
+        List<String> experimentados = competidores.stream()
+                .filter(e -> e.getMedallas() > 5)
+                .map(e -> e.getNombre() + "(" + e.getMedallas() + ")")
+                .toList();
+        System.out.println("Entrenadores con > 5 medallas: " + experimentados);
+    }
 }
