bd22f7ec101a76a6dc8c6b1d84c7e928d8e4f5c7
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:21:27 -0500

    Hice el ejercicio 14 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio14.java
+++ src/main/dosw/semana2/pokemon/Ejercicio14.java
@@ -1,4 +1,24 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 public class Ejercicio14 {
+    public static void main(String[] args) {
+        List<Pokemon> pokedex = Arrays.asList(
+                new Pokemon(1L, "Arcanine",  "Fuego",    65, 555, "Kanto",  false),
+                new Pokemon(2L, "Typhlosion","Fuego",    70, 534, "Johto",  false),
+                new Pokemon(3L, "Blaziken",  "Fuego",    68, 510, "Hoenn",  false),
+                new Pokemon(4L, "Infernape", "Fuego",    66, 518, "Sinnoh", false),
+                new Pokemon(5L, "Blastoise", "Agua",     72, 530, "Kanto",  false),
+                new Pokemon(6L, "Feraligatr","Agua",     69, 515, "Johto",  false)
+        );
+        Map<String, List<String>> porRegion = pokedex.stream()
+                .collect(Collectors.groupingBy(
+                        Pokemon::getRegion,
+                        Collectors.mapping(Pokemon::getNombre, Collectors.toList())
+                ));
+        porRegion.forEach((region, nombres) ->
+                System.out.println(region + ": " + nombres));
+    }
 }
