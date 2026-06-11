0a58ad759d615dd3704fe8b212f6e978997a0055
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:10:29 -0500

    Hice el ejercicio 5 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio5.java
+++ src/main/dosw/semana2/pokemon/Ejercicio5.java
@@ -1,4 +1,23 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Ejercicio5 {
+    record Criatura(String nombre, int nivel) {}
+    public static void main(String[] args) {
+        List<Criatura> equipo = Arrays.asList(
+                new Criatura("Arcanine",  58),
+                new Criatura("Articuno",  85),
+                new Criatura("Zapdos",    83),
+                new Criatura("Machamp",   62),
+                new Criatura("Moltres",   87),
+                new Criatura("Geodude",   35)
+        );
+        List<String> poderosos = equipo.stream()
+                .filter(c -> c.nivel() > 80)
+                .map(Criatura::nombre)
+                .collect(Collectors.toList());
+        System.out.println("Pokémon con nivel > 80: " + poderosos.size());
+        System.out.println(poderosos);
+    }
 }
