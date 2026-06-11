a96dc181e3a8eecc074323d86ec12cc42c551e50
Julianizz18 <904juliancorredor@gmail.com>
2026-06-11 11:18:57 -0500

    Hice el ejercicio 11 de la semana 2

--- src/main/dosw/semana2/pokemon/Ejercicio11.java
+++ src/main/dosw/semana2/pokemon/Ejercicio11.java
@@ -1,4 +1,20 @@
 package main.dosw.semana2.pokemon;
-
+import java.util.Arrays;
+import java.util.List;
 public class Ejercicio11 {
+    public static void main(String[] args) {
+        List<Pokemon> pokedex = Arrays.asList(
+                new Pokemon(1L, "Alakazam", "Psíquico",  55, 445, "Kanto", false),
+                new Pokemon(2L, "Articuno", "Hielo",     75, 580, "Kanto", true),
+                new Pokemon(3L, "Machamp",  "Lucha",     48, 390, "Kanto", false),
+                new Pokemon(4L, "Zapdos",   "Eléctrico", 78, 620, "Kanto", true),
+                new Pokemon(5L, "Geodude",  "Roca",      22, 175, "Kanto", false),
+                new Pokemon(6L, "Arcanine", "Fuego",     65, 555, "Kanto", false)
+        );
+        double promedio = pokedex.stream()
+                .mapToDouble(Pokemon::getPoderCombate)
+                .average()
+                .orElse(0);
+        System.out.printf("Poder de combate promedio: %.2f%n", promedio);
+    }
 }
